5cca4cd358ce2a0d5ef4d28b8172ee7da1bb3649
massis <massis@massis-256-5.local>
2026-02-09 22:55:38 +0800

    paths: use cwd as root

--- src/paths.ts
+++ src/paths.ts
@@ -1,16 +1,9 @@
-import fs from 'fs';
-import path from 'path';
-
 /**
  * Returns the project root path.
  *
- * This can be different depending on whether compiled scripts (i.e. in dist/)
- * are used, or, in jest's case, whether the .ts files are being run directly
- * via ts-jest.
+ * Gateway expects to be started from the project root, so `process.cwd()` is a
+ * reliable root path across dev, tests, and production containers.
  */
 export function rootPath(): string {
-  const insideDistDir: boolean = __filename.match(/dist\//) !== null;
-  // Return absolute path to project root, always pointing to /Users/feng/gateway
-  // regardless of environment
   return process.cwd();
 }
